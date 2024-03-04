735fc10b16498e1e1dbd5839dfbb6c0be3b0717b
Brendan <daly2b@gmail.com>
2024-03-04 12:27:40 -0500

    Fixed bug with ptofile pic

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -2,7 +2,7 @@ export default function AboutMe() {
   return (
     <div className="flex flex-row items-center space-x-4">
       
-      <img className="box-content p-4 rotate-90" src="/assets/images/myPic.png" alt="Brendan Daly" style={{ width: '100px' }} />
+      <img className="box-content p-4" src="/assets/images/myPic.png" alt="Brendan Daly" style={{ width: '100px' }} />
       <p className="flex-1">
         Holding a BA in History from the College of William and Mary, my academic journey has been diverse and fulfilling. This foundational education was further enriched by my engagement in several data science classes, where I developed a keen interest in analytical methodologies and techniques. My passion for technology and its vast potential led me to pursue a certification from a coding bootcamp through Penn, equipping me with the skills to navigate and contribute to the dynamic field of software development. My journey weaves together a love for history, a knack for data analysis, and a proficiency in coding, preparing me for a unique and impactful career path.
       </p>
