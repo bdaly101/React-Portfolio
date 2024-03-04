0d0b1cdb4aaf6a70d23ca617d12354d375b2354d
Brendan <daly2b@gmail.com>
2024-03-04 12:26:46 -0500

    Fixed bugs with footer images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ function Footer() {
     <footer className="footer" style={{ display: 'flex', justifyContent: 'center', gap: '20px', alignItems: 'center', padding: '20px' }}>
       <p>Find me on:</p>
       <a href="https://github.com/bdaly101" target="_blank" rel="noopener noreferrer">
-        <img src="./public/assets/github.svg" alt="GitHub" style={{ width: '24px', height: '24px' }} />
+        <img src="/assets/github.svg" alt="GitHub" style={{ width: '24px', height: '24px' }} />
       </a>
       <a href="https://www.linkedin.com/in/brendan-daly-a7a481278/" target="_blank" rel="noopener noreferrer">
-        <img src="../public/assets/linkedin.svg" alt="LinkedIn" style={{ width: '24px', height: '24px', color: 'white' }} />
+        <img src="/assets/linkedin.svg" alt="LinkedIn" style={{ width: '24px', height: '24px', color: 'white' }} />
       </a>
     </footer>
   );
